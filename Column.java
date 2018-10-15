4dcbcf6dc37758f8ee623ecd781a8d3ef29dfa44
Chendyreva <nat-217@mail.ru>
2018-10-15 17:46:13 +0500

    class Column

---
+++ Column.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface Column {
+    String field();
+    double data();
+    long time() default 123;
+}
