7523a9fc2936aa03858e22259a9ad58fcdc65ccc
Chendyreva <nat-217@mail.ru>
2018-10-15 17:46:48 +0500

    LogParams

---
+++ LogParams.java
@@ -0,0 +1,8 @@
+package ru.geekbrains;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
+public @interface LogParams {
+}
